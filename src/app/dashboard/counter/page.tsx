fe9c82f38d457cee837efac805200e9644974bd1
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-12 15:54:32 -0500

    Counter - Manejo de estado - useState

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,35 @@
+'use client';
+
+import { Metadata } from "next";
+import { useState } from "react";
+
+
+export const metadata: Metadata = {
+ title: 'Counter page',
+ description: 'Un contador',
+};
+
 export default function CounterPage() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <span className="text-9xl">{count}</span>
+
+      <div className="flex">
+        <button 
+          onClick={() => setCount( count - 1 )}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+          -1
+        </button>
+        <button 
+          onClick={() => setCount( count + 1 )}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
+          +1
+        </button>
+      </div>
     </div>
   );
 }

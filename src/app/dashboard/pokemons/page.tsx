5c608fae86dcc93bf1111a91a8138279c3bd9776
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-12 19:00:21 -0500

    Pensemos en componentes pequeños

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,38 +1,33 @@
-import { PokemonsResponse, SimplePokemon } from "@/app/dashboard/pokemons";
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/dashboard/pokemons";
 import Image from "next/image";
 
-const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
-    const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
-        .then(res => res.json());
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
-    const pokemons = data.results.map( pokemon => ({
-        id: pokemon.url.split('/').at(-2)!, 
-        name: pokemon.name,
-    }) );
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
 
-    return pokemons;
+  return pokemons;
 };
 
 export default async function PokemonsPage() {
-
-    const pokemons = await getPokemons(151);
+  const pokemons = await getPokemons(151);
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {
-            pokemons.map( pokemon => (
-                <Image 
-                    key={pokemon.id}
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} 
-                    width={100}
-                    height={100}
-                    alt={pokemon.name}
-                    title={pokemon.name}
-                />
-            ) )
-        }
-      </div>
+      <span className="text-5xl my-2">
+        Listado de Pokemons <small>estatico</small>
+      </span>
+      
+        <PokemonGrid pokemons={pokemons} />
+
     </div>
   );
 }

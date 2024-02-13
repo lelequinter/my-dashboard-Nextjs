48b60f63faba5c45417ca9b7ed076b640100035f
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-13 11:39:04 -0500

    Metadata dinámica

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,25 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata>{
+    const {id, name} = await getPokemon(params.id);
+
+    return {
+        title: `#${id}-${name}`,
+        description: `Página del pokemon ${name}`
+    }
+}
+
 const getPokemon = async (id: string): Promise<Pokemon> => {
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-        cache: 'force-cache' //todo: cambiar esto en un futuro
+        // cache: 'force-cache' //todo: cambiar esto en un futuro
+        next: {
+            revalidate: 60,
+        }
     }).then( res => res.json());
 
     console.log('se cargo',pokemon.name);
@@ -19,7 +32,7 @@ export default async function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
-      <div> {JSON.stringify(pokemon)} </div>
+      <div> {pokemon.name} </div>
     </div>
   );
 }

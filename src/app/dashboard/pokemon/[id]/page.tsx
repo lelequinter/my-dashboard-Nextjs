700ec502a625f0642e356f04a53e9658a108e2f3
Leyder Quintero <desarrollador9@wedevel.co>
2024-02-13 11:31:42 -0500

    Cargar información del Pokémon por ID

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,11 +1,25 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
   params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        cache: 'force-cache' //todo: cambiar esto en un futuro
+    }).then( res => res.json());
+
+    console.log('se cargo',pokemon.name);
+    return pokemon;
+}
+
+export default async function PokemonPage({ params }: Props) {
+    const pokemon = await getPokemon(params.id);
+
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div> {JSON.stringify(pokemon)} </div>
     </div>
   );
 }
